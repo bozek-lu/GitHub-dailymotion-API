d2aac58cd3e1b8dbd4a84a310a0e42fe3a71c6d0
Łukasz Bożek <bozek.lu@gmail.com>
2018-06-05 20:15:07 +0200

    added extensions

---
+++ GitHub-dailymotion-API/GitHub-dailymotion-API/Common/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extensions.swift
+//  GitHub-dailymotion-API
+//
+//  Created by Łukasz Bożek on 05/06/2018.
+//  Copyright © 2018 lu. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static func db_red() -> UIColor {
+        return UIColor(red: 244/255, green: 78/255, blue: 80/255, alpha: 1)
+    }
+}
---
+++ GitHub-dailymotion-API/GitHub-dailymotion-API/Common/Extensions/UIFont+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIFont+Extensions.swift
+//  GitHub-dailymotion-API
+//
+//  Created by Łukasz Bożek on 05/06/2018.
+//  Copyright © 2018 lu. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    static func db_default() -> UIFont {
+        return UIFont.systemFont(ofSize: 40)
+    }
+}
